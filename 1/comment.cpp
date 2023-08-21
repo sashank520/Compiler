c988c3935b32916efe577ec4c54e8e2f382a2b31
Muraja10 <112752028+Muraja10@users.noreply.github.com>
2023-08-21 13:14:12 +0530

    Add files via upload

---
+++ 1/comment.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main()   
+{
+	char str[20];
+	printf("enter the string: ");
+	scanf("%s",&str);
+	int len=strlen(str);
+	if(str[0]=='/' && str[1]=='/' || (str[0]=='/' && str[1]=='*' && str[len-1]=='/' && str[len-2]=='*')) {
+		printf("This is a comment");
+	}      
+	else    {
+		printf("This is not a comment"); 
+	}
+	return 0;
+}
